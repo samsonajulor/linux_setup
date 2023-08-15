06cb7002b691589524dfd85d5f3455c7ca7395f8
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 07:23:42 +0100

    update

--- node_nvm.sh
+++ node_nvm.sh
@@ -5,9 +5,20 @@ echo "Installing Node.js and NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 # Load NVM into this shell session
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js using NVM
-nvm install node
+source ~/.zshrc
+clear
+
+echo 'Installing NodeJS LTS'
+nvm --version
+nvm install --lts
+nvm current
 
