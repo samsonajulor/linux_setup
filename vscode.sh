b6c10ce8d60b4327364d58a40fff0dc9c6405504
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 07:19:41 +0100

    update

--- vscode.sh
+++ vscode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Installing Visual Studio Code..."
-sudo apt-get update
-sudo apt-get install code
+echo 'Installing VSCode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update && sudo apt-get install code -y
+
+echo 'Installing Code Settings Sync'
+code --install-extension Shan.code-settings-sync
+sudo apt-get install gnome-keyring -y
+cls
--- yarn.sh
+++ yarn.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Installing Yarn..."
+echo 'Installing Yarn'
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update
-sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+echo '"--emoji" true' >> ~/.yarnrc
+
+echo 'Installing Typescript'
+yarn global add typescript
+clear
