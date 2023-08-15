a266ce1240952aa8ca4254e08e84d9c28b6baf79
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 08:18:51 +0100

    update

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-echo "Welcome! Let's start setting up your system. It could take more than 10 minutes, be patient"
+echo "Welcome! Let's start with some questions"
 
 echo "What name do you want to use in GIT user.name?"
-echo "For example, mine will be \"Luke Morales\""
 read git_config_user_name
 
 echo "What email do you want to use in GIT user.email?"
-echo "For example, mine will be \"lukemorales@live.com\""
 read git_config_user_email
 
 echo "What is your github username?"
-echo "For example, mine will be \"lukemorales\""
 read username
 
 cd ~ && sudo apt-get update
--- vscode.sh
+++ vscode.sh
@@ -2,4 +2,3 @@
 
 echo 'Installing VSCode'
 sudo snap install --classic code
-clear
