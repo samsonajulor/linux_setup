0a3226c7aed53b3f9e932ef9357f5532416e40a0
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 07:14:55 +0100

    update

--- node_nvm.sh
+++ node_nvm.sh
@@ -3,5 +3,11 @@
 
 echo "Installing Node.js and NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.bashrc
+
+# Load NVM into this shell session
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js using NVM
+nvm install node
 
