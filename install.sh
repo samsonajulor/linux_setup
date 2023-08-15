47fcb2368ed094cf8fb6017440bb2c3e3eba045d
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 08:29:57 +0100

    update

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define function to install a tool using its script
+install_program() {
+    tool_script="$1.sh"
+    if [ -f "$tool_script" ]; then
+        sh "$tool_script"
+    else
+        echo "Script $tool_script not found!"
+    fi
+}
+
+# List of tools to install
+tools=("start" "aws" "git" "dbeaver" "postgresql" "zsh" "node_nvm" "terraform" "slack" "yarn" "vscode" "discord")
+
+# Loop through the list and install each tool
+for tool in "${tools[@]}"; do
+    install_program "$tool"
+done
