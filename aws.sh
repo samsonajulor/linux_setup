b56fe91b6aa5e1df31c8b221242522e4172b850e
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 06:49:16 +0100

    update

--- aws.sh
+++ aws.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 echo "Installing AWS CLI..."
+sudo yum remove awscli -y
 sudo apt-get update
-sudo apt-get install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo aws --version
+
+echo "aws installation complete..."
+
+# echo "configuring aws cli..." do this manually
+# sudo aws configure sso
+
+# follow the instructions here https://docs.aws.amazon.com/cli/latest/userguide/getting-started-quickstart.html
