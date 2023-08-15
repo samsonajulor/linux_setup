9affca9665933da966cbe02a75e7ad97079786db
Samson Ajulor <samsonajulor@gmail.com>
2023-08-15 06:54:49 +0100

    update

--- node_nvm.sh
+++ node_nvm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# source: https://tecadmin.net/how-to-install-nvm-on-ubuntu-20-04/
 
 echo "Installing Node.js and NVM..."
+sudo apt install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
-nvm install node
+sudo nvm install node
+sudo nvm install --lts 
+sudo nvm ls
